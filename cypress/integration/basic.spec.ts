be3f7fc541d10ba0ad29759365a83c27f64ef93a
Dewey Ou <oushihao97@gmail.com>
2022-04-27 18:12:58 +0800

    test: e2e test

--- cypress/integration/basic.spec.ts
+++ cypress/integration/basic.spec.ts
@@ -9,21 +9,7 @@ context('Basic', () => {
     cy.url()
       .should('eq', 'http://localhost:3000/')
 
-    cy.contains('p', 'Home')
+    cy.contains('p', 'Todo List')
       .should('exist')
-    cy.contains('p', 'About')
-      .should('exist')
-
-    cy.contains('p', 'Count: 0')
-    cy.get('button[btn]')
-      .click()
-    cy.contains('p', 'Count: 1')
-  })
-
-  it('nav', () => {
-    cy.contains('p', 'About')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3000/about')
   })
 })
